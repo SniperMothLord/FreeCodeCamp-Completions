ccdc9fda3b5facee53e82d86b8673137b1c42737
SniperMothLord <snipermothlord@gmail.com>
2025-06-16 15:44:13 +0300

    Dydaduu
    dur dur dur

---
+++ Certified FullStack Curriculum/3. Javascript/Variables and Strings/Sentence Maker/script.js
@@ -0,0 +1,25 @@
+let adjective, noun, verb, place, adjective2, noun2;
+
+adjective = "fabulous";
+noun = "hacker";
+verb = "hacking";
+place = "neighbourhood";
+adjective2 = "sexy";
+noun2 = "rapist";
+
+let firstStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+
+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("First story: "+firstStory);
+
+adjective = "fabulous";
+noun = "hacker";
+verb = "hornying";
+place = "neighbourhood";
+adjective2 = "sexy";
+noun2 = "rapist";
+
+let secondStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+
+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("Second story: "+secondStory);
---
+++ Certified FullStack Curriculum/3. Javascript/Variables and Strings/Trivia Bot/script.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Kalle"
+let botLocation = "Your mom";
+let favoriteLanguage = "Speaking french between your fathers legs";
+
+console.log("My name is "+botName+" and I live on "+botLocation+".");
+console.log("My favorite programming language is "+favoriteLanguage+".");
+
+let codingFact = "Did you know that learning "+favoriteLanguage+" can indeed ncrease your productivity";
+console.log(codingFact);
+
+codingFact = favoriteLanguage+" is one of the oldest programming langugages in the world!";
+console.log(codingFact);
+
+codingFact = "I am your father! "+codingFact;
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
